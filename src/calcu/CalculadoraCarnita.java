864fb29da23a1fa5a127c8cdaf24572977d0823d
MRUIZGA <MRUIZGA@112Salas08.inge.itam.mx>
2015-02-18 10:45:16 -0600

    Proyecto para hacer una calculadora

---
+++ src/calcu/CalculadoraCarnita.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcu;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author MRUIZGA
+ */
+public class CalculadoraCarnita {
+    public Stack<Double> pila;
+    
+    public CalculadoraCarnita() {
+        this.pila=new Stack <Double>();
+    }
+    
+    public double Evaluapostfijo(String num){
+        double res=0;
+        char 
+        
+        
+        return res;
+    }
+}
