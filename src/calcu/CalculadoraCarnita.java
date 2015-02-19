1652f1f1a8f5bf0f37c3a44905ffcf4efa434024
introduccion02 <introduccion02@lmint13.iio2.itam.mx>
2015-02-19 13:11:38 -0600

    funcion usando miPila

--- src/calcu/CalculadoraCarnita.java
+++ src/calcu/CalculadoraCarnita.java
@@ -7,70 +7,68 @@ package calcu;
 
 import java.util.Scanner;
 import java.util.Stack;
+import stackadt.StackADT;
 import stackadt.Unchecked;
+import stackadt.miPila;
 
 /**
  *
  * @author MRUIZGA
  */
 public class CalculadoraCarnita {
+
     public Stack<Double> pila;
-    
+
     public CalculadoraCarnita() {
-        this.pila=new Stack <Double>();
+        this.pila = new Stack<Double>();
     }
-    
-    public double Evaluapostfijo(String operador)throws Unchecked{
-         Stack <Double> calcu=new Stack<Double>();
-        double res=0,num1,num2;
+
+    public double Evaluapostfijo(String operador) throws Unchecked {
+        miPila calcu = new miPila();
+        double res = 0, num1, num2;
         char aux;
         String auxS;
-        Scanner sc=new Scanner(operador);
-        
-        while(sc.hasNext()){
-            auxS=sc.next();
-            aux=auxS.charAt(0);
-            if(aux!='/' && aux!='*' && aux!='-' && aux!='+'){
-                calcu.add(Double.parseDouble(auxS));
-            }
-            else{
-                num2=calcu.peek();
-                calcu.pop();
-                num1=calcu.peek();
-                calcu.pop();
-                switch (aux){
+        Scanner sc = new Scanner(operador);
+
+        while (sc.hasNext()) {
+            auxS = sc.next();
+            aux = auxS.charAt(0);
+            if (aux != '/' && aux != '*' && aux != '-' && aux != '+') {
+                calcu.push(Double.parseDouble(auxS));
+            } else {
+                num2 = (Double) calcu.pop();
+                num1 = (Double) calcu.pop();
+                switch (aux) {
                     case '/':
-                        if(num2==0){
-                            throw new Unchecked("No puedes dividir entre 0");
-                        }
-                        else{
-                            res=num1/num2;
+                        if (num2 == 0) {
+                            throw new Unchecked("Math ERROR");
+                        } else {
+                            res = num1 / num2;
                         }
                         break;
                     case '*':
-                        res=num1*num2;
+                        res = num1 * num2;
                         break;
                     case '-':
-                        res=num1-num2;
+                        res = num1 - num2;
                         break;
                     case '+':
-                        res=num1+num2;
+                        res = num1 + num2;
                 }
-                 calcu.add(res);
+                calcu.push(res);
             }
         }
-        if(calcu.size()!=1){
-            throw new Unchecked ("La operacion no tiene el formato correcto.");
+        if (calcu.size() != 1) {
+            throw new Unchecked("Syntax ERROR");
         }
-                
-        return calcu.peek();
+
+        return (Double) calcu.peek();
     }
-    
+
     public static void main(String[] args) {
-        String operador="1.2 8 / ";
-        CalculadoraCarnita calcu=new CalculadoraCarnita();
-        
-        System.out.print("="+calcu.Evaluapostfijo(operador));
+        String operador = "1.2 8 / 15 * 9 -";
+        CalculadoraCarnita calcu = new CalculadoraCarnita();
+
+        System.out.print("=" + calcu.Evaluapostfijo(operador));
     }
-    
 }
